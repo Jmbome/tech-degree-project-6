97b2f7d7e75645ce4be1007cfff9432db5011b68
Joe Mbome <joembome@yahoo.com>
2021-03-03 15:01:37 -0500

    update

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const qwerty=document.getElementById('qwerty');
 const phrases=document.getElementById('phrase');
 const startButton=document.querySelector('.btn__reset');
 const overLay=document.getElementById('overlay'); 
+const button=document.getElementsByTagName('button');
+const heart=document.querySelector('.tries')
 const phraseList=[
     'A bird in the hand is worth two in the bush',
     'A bunch of fives',
@@ -32,10 +34,9 @@ getRandomPhraseAsArray(phraseList);
 
 //adds the letters of a string to the display
 const addPhraseToDisplay=(arr)=>{
-    let items='';
     let ul = phrases.querySelector('ul');
-    let li = document.createElement("li");
     for(let i=0; i<arr.length; i++){
+     let li = document.createElement("li");
      li.textContent = arr[i];  
      ul.appendChild(li);
      if(arr[i]=== " "){
@@ -43,9 +44,48 @@ const addPhraseToDisplay=(arr)=>{
      }else{
         li.classList.add("letter");
      } 
-   }
-    
+   }  
      
 }
-
 const phraseArray = getRandomPhraseAsArray(phraseList);
+
+//check if a letter is in the phrase
+
+const checkLetter=(button)=>{
+    let liList=ul.children();
+    let matchFound=0;
+    for(i=0;i<button.length;i++){
+     liList[i][0];
+     if(button.textContent===li.textContent){
+         li.classlist.add('show');
+         matchFound=button.textContent;
+         return match;
+        }
+      }
+    }
+
+    //Listen for the onscreen keyboard to be clicked
+
+    qwerty.addEventListener('click',(e)=>{
+        let checked='';
+        var missed=0;
+      if(e.target.tagName=="button"){
+       button.classlist.add('chosen');
+      checked=checkLetter();
+       if(checked==0){
+         heart-=1;
+         missed +=1;
+       }
+      }
+
+    } )
+
+    const letterList=document.querySelector('.letter');
+    const showList=document.querySelector('.show');
+    const winOverlay=overLay.classList.add("win");
+    const headLineText=document.querySelector('.title')
+
+    if(letterList.length==showList.length){
+         winOverLay;
+         headLineText=`"YOU WON!!!"`;
+    }
